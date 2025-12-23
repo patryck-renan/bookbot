e5b7ea87fe909786c5e85abee3f428f28b604683
patryck-renan <patryck.renan@gmail.com>
2025-12-22 21:56:46 -0300

    update stats.py w/ sort_dict func

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from stats import *
 def main():
     book = get_book_text("./books/frankenstein.txt")
     number_words = count_words(book)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {number_words} total words")
+    print("--------- Character Count -------")
     dict_characters = count_characters(book)
-    print(dict_characters)
+    lista = sort_dict(dict_characters)
+    for i in lista:
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -19,3 +19,14 @@ def count_characters(book_content):
             dict_characters.setdefault(f"{word}",1)
 
     return dict_characters
+
+def sort_dict(dict):
+    lista_dicts = []
+    for char, count in dict.items():
+        if char not in lista_dicts:
+            lista_dicts.append({"char":char, "num":count})
+    def sort_list(items):
+        return items["num"]
+    lista_dicts.sort(reverse=True, key=sort_list)
+
+    return lista_dicts
